4c311d65926283a5cf604ed0005bc7ed6deddd64
Squirrel7ang <925071399@qq.com>
2025-07-13 11:15:14 +0800

    [feat] finish cpp SGD with bugs

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 # NOTE: on MacOS you need to add an addition flag: -undefined dynamic_lookup
 default:
 	c++ -O3 -Wall -shared -std=c++11 -fPIC $$(python3 -m pybind11 --includes) src/simple_ml_ext.cpp -o src/simple_ml_ext.so
+
+.PHONY: clean
+clean:
+	rm src/simple_ml_ext.so
--- src/simple_ml_ext.cpp
+++ src/simple_ml_ext.cpp
@@ -5,6 +5,68 @@
 
 namespace py = pybind11;
 
+template <typename T>
+void dot(T* X, T* Y, T* result, int m, int n, int p) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            result[i*p+j] = 0;
+            for (int k = 0; k < n; k++) {
+                result[i*p+j] += X[i*n+k] * Y[k*p+j];
+            }
+        }
+    }
+}
+
+template <typename T>
+void transpose(T* X, T* result, int m, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            result[i*m+j] = X[j*n+i];
+        }
+    }
+}
+
+void exp(float* X, float* result, int m, int n) {
+    for (int i = 0; i < m * n; i++) {
+        result[i] = (float) std::exp((double) X[i]);
+    }
+}
+
+template <typename T>
+void normalize_by_row(T* X, T* result, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        T sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum = sum + X[i*n+j];
+        }
+        for (int j = 0; j < n; j++) {
+            result[i*n+j] = X[i] / sum;
+        }
+    }
+}
+
+template <typename T>
+void sub(T* X, T* Y, T* result, int m, int n) {
+    for (int i = 0; i < m * n; i++) {
+        result[i] = X[i] - Y[i];
+    }
+}
+
+template <typename S, typename T>
+void mul(S factor, T* X, T* result, int m, int n) {
+    for (int i = 0; i < m*n; i++) {
+        result[i] = factor * X[i];
+    }
+}
+
+void gen_one_hot(float* result, const unsigned char* position, int m, int n) {
+    for (int i = 0; i < m * n; i++) {
+        result[i] = 0.0;
+    }
+    for (int i = 0; i < m; i++) {
+        result[i*n + (int)position[i]] = 1.0;
+    }
+}
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
@@ -33,7 +95,23 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    float* Z_batch  = new float[m * k];
+    float* Iy_batch = new float[m * k];
+    float* grad     = new float[m * k];
+    for (int i = 0; i < (int)m; i += batch) {
+        int batch_size = (i + batch > m) ? (m - i) : batch; 
 
+        const float* X_batch = X + batch_size * n;
+        const unsigned char* y_batch = y + batch_size;
+
+        dot<float>((float*)X_batch, theta, Z_batch, m, n, k);
+        exp(Z_batch, Z_batch, m, k);
+        normalize_by_row<float>(Z_batch, Z_batch, m, k);
+        gen_one_hot(Iy_batch, y_batch, m, k);
+        sub(Z_batch, Iy_batch, grad, m, k);
+        mul(lr / batch_size, grad, grad, m, k);
+        sub(theta, grad, theta, m, k);
+    }
     /// END YOUR CODE
 }
 
